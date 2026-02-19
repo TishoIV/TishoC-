219282559cd8f809ac200ae55f4a389073a2d5bf
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-19 09:31:38 +0200

    Add files via upload

---
+++ ConsoleApp1tisho/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1tisho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int r = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            double s = Math.PI * Math.Pow(r,2);
+            int s2 = a * b;
+            int s3 = (a * h) / 2;
+            int s4 = a * h;
+            int s5 = (a + b) * 2 / h;
+
+
+
+            Console.WriteLine($"Лицето на правоъгълник:{s2}");
+            Console.WriteLine($"Лице на кръг:{s}");
+            Console.WriteLine($"Лице на триъгълник:{s3}");
+            Console.WriteLine($"Лице на успоредник:{s4}");
+            Console.WriteLine($"Лице на трапец:{s5}");
+
+
+
+
+
+        }
+    }
+}
