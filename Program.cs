4197bd9c5594507bb1dae87e28aa7252d522ed94
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-19 09:41:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TishoConv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mm = int.Parse(Console.ReadLine());
+            double m = mm / 1000;
+            Console.WriteLine($"Милиметри в метри:{m}");
+
+            int kg = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
