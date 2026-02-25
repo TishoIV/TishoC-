e03acf141145b36ec74a9a85445c5e5b196f4821
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-25 19:11:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿//Програма коята пресмята  средния разход на автомобил за изминати 100 км. Обема на резорвоар за гориво и модела на автомобил се въвежда от потребитяла
+//След пресмятането на разхода да се изведе следното съобщение: "Вашият среден разход е 100 км е 10 л/км, все пак е АУДИ"
+using System;
 
-namespace TishoConv
+namespace SredenRazhodAvt
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int mm = int.Parse(Console.ReadLine());
-            double m = mm / 1000;
-            Console.WriteLine($"Милиметри в метри:{m}");
+            Console.Write("Въведете марката на автомобила: ");
+            string model = Console.ReadLine();
 
-            int kg = int.Parse(Console.ReadLine());
+            Console.Write("Въведете изразходваното гориво в литри за 100 км: ");
+            double litri = double.Parse(Console.ReadLine());
 
+            int km = 100;
 
+            double sredenRazhod = (litri / km) * 100;
+
+            Console.WriteLine($"Вашият среден разход за 100 км е {sredenRazhod:F2} л/100км, все пак е {model}");
         }
     }
 }
