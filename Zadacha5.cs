e32b79153f2c495d986cdf535ab10d199599aded
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-26 09:41:36 +0200

    Add files via upload

---
+++ Zadacha5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadacha5
+{
+    internal class Zadacha5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи число: ");
+            int n = int.Parse(Console.ReadLine());
+            int faktoriel = 1;
+            for(int i = 1; i<=n; i++)
+            {
+                faktoriel *= i;
+            }
+            Console.WriteLine("");
+
+        }
+    }
+}
