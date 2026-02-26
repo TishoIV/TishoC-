ace0e5f3abb49c9138d27dca431f40987bf6551c
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-26 09:27:01 +0200

    Add files via upload

--- Zadacha4.cs
+++ Zadacha4.cs
@@ -7,14 +7,13 @@ namespace Zadacha4
         static void Main(string[] args)
         {
             Console.WriteLine("Числа между 1-100, които се делят на 7: ");
-            for (int i = 1; i<=91 ; i++)
+            for (int i = 1; i<100 ; i++)
             {
                 if (i % 7 == 0)
                 {
-
+                    Console.WriteLine(i);
                 }
-                int res = i / 7;
-                Console.WriteLine(res);
+               
             }
 
         }
