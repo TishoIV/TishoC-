a8dcbbbddb3c179d6c8d2d3ab7c368edc8d6cc38
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-02-26 09:16:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
-﻿//Програма коята пресмята  средния разход на автомобил за изминати 100 км. Обема на резорвоар за гориво и модела на автомобил се въвежда от потребитяла
-//След пресмятането на разхода да се изведе следното съобщение: "Вашият среден разход е 100 км е 10 л/км, все пак е АУДИ"
+﻿
 using System;
 
-namespace SredenRazhodAvt
+namespace Zadacha3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Въведете марката на автомобила: ");
-            string model = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Прав ред: ");
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + "");
+            }
+            Console.WriteLine("Обратен ред: ");
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i+ "");
 
-            Console.Write("Въведете изразходваното гориво в литри за 100 км: ");
-            double litri = double.Parse(Console.ReadLine());
-
-            int km = 100;
-
-            double sredenRazhod = (litri / km) * 100;
-
-            Console.WriteLine($"Вашият среден разход за 100 км е {sredenRazhod:F2} л/100км, все пак е {model}");
+            }
         }
     }
 }
