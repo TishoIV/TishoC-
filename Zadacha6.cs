78d9c95e263eba0614bcc47998f3a782a15a30c9
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-03-04 19:42:17 +0200

    Add files via upload

---
+++ Zadacha6.cs
@@ -0,0 +1,50 @@
+﻿//Приложение на C#, с което се намира квадратно уравнение от вида ax^2 + bx + c = 0. Проверка за дали уравнението е квадратно т.е а различно от 0.
+
+using System;
+
+namespace Zadachki
+{
+    internal class Zadacha6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a= ");
+            double a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b= ");
+            double b = int.Parse(Console.ReadLine());
+            Console.WriteLine("c= ");
+            double c = int.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Уравнението не е квадратно");
+            }
+            else
+            {
+                Console.WriteLine("Уравнението е квадратно");
+
+
+
+
+            }
+            double discriminanta = b * b - 4 * a * c;
+            Console.WriteLine($"Дискриминанта (D) = {discriminanta}");
+            if (discriminanta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminanta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminanta)) / (2 * a);
+                Console.WriteLine($"x1={x1}");
+                Console.WriteLine($"x1={x2}");
+            }
+            else if (discriminanta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Уравнението има един корен: x = {x}");
+            }
+            else
+            {
+                Console.WriteLine("Уравнението няма реални корени");
+            }
+        }
+    }
+}
+   
---
+++ Zadacha7.cs
@@ -0,0 +1,30 @@
+﻿//Приложенеи коете се намира сумата на  произволно въведени положителни числа от клавиатурата. Въвеждането на стойности 0 да прекрати по нататъшното въвеждане на числа.
+using System;
+
+namespace Zadachki
+{
+    internal class Zadacha7
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            while(true)
+            {
+                Console.WriteLine("Въведете положително число (0 за край): ");
+                double number = double.Parse(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Моля, въведете положително число.");
+                    continue;
+                }
+                sum += number;
+            }
+            Console.WriteLine($"Сумата на въведените положителни числа е: {sum}");
+
+        }
+    }
+}
