865e0ae7981237651cdaac1cc43308e450e12cdf
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-03-12 09:43:02 +0200

    ZadachaMasiv6
    Updated the data structure to combine station names and temperatures into a single array.

--- ZadachaMasiv6.cs
+++ ZadachaMasiv6.cs
@@ -1,44 +1,26 @@
-﻿using System;
+using System;
 
 namespace ZadachaMasiv6
 {
-    internal class Program
+    internal class ZadachaMasiv6
     {
         static void Main(string[] args)
         {
-            string[,] stanciq = new string[9,1]
+            string[,] stanciq = new string[9, 2]
             {
-                {"Ново село"},
-                {"Видин" },
-                {"Лом" },
-                {"Оряхово" },
-                {"Монтана" },
-                {"Враца" },
-                {"Кнежа" },
-                {"Ловеч" },
-                {"Плевен" },
+                {"Ново село","8"},
+                {"Видин", "8"},
+                {"Лом", "8"},
+                {"Оряхово", "8" },
+                {"Монтана", "15" },
+                {"Враца", "14" },
+                {"Кнежа", "8" },
+                {"Ловеч", "10" },
+                {"Плевен", "13" },
+              };
 
-
-            };
-            string[,] temp = new string[9, 1]
-            {
-                {"8 " },
-                {"8 " },
-                {"8 " },
-                {"8 " },
-                {"15 " },
-                {"14 " },
-                {"8 " },
-                {"10 " },
-                {"13 " },
-            };
-            for (int i = 0; i < 9; i++)
-            {
-                Console.WriteLine($"{stanciq[i, 0]} с температура {temp[i, 0]} градуса");
-            }
-
-
+           
         }
     }
-}
+
 
