85d6474b7d2f0c71ba9e009365a34206bf19ac91
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-03-12 09:42:13 +0200

    Add files via upload

---
+++ Zadacha3Masivt.cs
@@ -0,0 +1,49 @@
+﻿//Приложение с което се дефинира едномерен масив от n брой дробно-десетични елемента.
+using System;
+
+namespace ZadachaMasiv3
+{
+    internal class Zadacha3Masivt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете n: ");
+           int n = int.Parse(Console.ReadLine());
+           double[] masiv = new double [n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Елемент[{i}]");
+                masiv[i] = int.Parse(Console.ReadLine());
+            }
+            double min = masiv[0];
+            double max = masiv[0];
+            int pozmin = 0;
+            int pozmax = 0;
+            double sum = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (masiv[i] < min) // Намиране на най-малка стойност
+                {
+                   min = masiv[i];
+                   pozmin = i;
+                }
+                if (masiv[i] > max) //Намиране най-голяма стойност
+                {
+                    max = masiv[i];
+                    pozmax = i;
+                }
+                sum += masiv[i];
+
+            }
+
+            Console.WriteLine($"Стойноста е: {min} на позиция {pozmin}");
+            Console.WriteLine($"Стойноста е: {max} на позиция {pozmax}");
+            Console.WriteLine($"Сумата е: {sum}");  
+
+
+
+
+        }
+    }
+}
---
+++ ZadachaMasiv6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ZadachaMasiv6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] stanciq = new string[9,1]
+            {
+                {"Ново село"},
+                {"Видин" },
+                {"Лом" },
+                {"Оряхово" },
+                {"Монтана" },
+                {"Враца" },
+                {"Кнежа" },
+                {"Ловеч" },
+                {"Плевен" },
+
+
+            };
+            string[,] temp = new string[9, 1]
+            {
+                {"8 " },
+                {"8 " },
+                {"8 " },
+                {"8 " },
+                {"15 " },
+                {"14 " },
+                {"8 " },
+                {"10 " },
+                {"13 " },
+            };
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"{stanciq[i, 0]} с температура {temp[i, 0]} градуса");
+            }
+
+
+        }
+    }
+}
+
