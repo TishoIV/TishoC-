5e503ea2f28de9f3d70ccce81dae368a1413974d
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-03-18 18:13:54 +0200

    Add files via upload

---
+++ LokFunkc1.cs
@@ -0,0 +1,29 @@
+﻿//Да се запише локална функция, която приема едномерен масив и извърша действието на елементите на масива(принтира).
+
+using System;
+
+namespace Zadacha3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masiv = { 4,5,6,7,8,9 };
+            void Printmasiv(int[] masiv2)
+            {
+                foreach (int element in masiv2)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+            Printmasiv(masiv);
+        
+          
+
+
+
+
+
+        }
+    }
+}
