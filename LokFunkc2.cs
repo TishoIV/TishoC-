e15877f0e6091a484da54eaba58ee847c671e0de
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-03-18 18:21:13 +0200

    Add files via upload

---
+++ LokFunkc2.cs
@@ -0,0 +1,25 @@
+﻿//Да се напише статичен метод, който приема един елемент цяло число и звежда таблицата за умножение с това число.
+
+using System;
+using System.Diagnostics;
+
+namespace Zadacha3
+{
+    internal class LokFunkc1
+    {
+        static void moqtMethod(int chislo)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{chislo} x {i} = {chislo * i}");
+            }
+        }
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Въведете число:");
+            int chislo = int.Parse(Console.ReadLine());
+            moqtMethod(chislo);
+
+        }
+    }
+}
