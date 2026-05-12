e4cd1ed8a95764eb1262ed4b06c41d6fe5a25564
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-05-12 22:23:40 +0300

    Promqna main program

--- Lab10.program.cs
+++ Lab10.program.cs
@@ -1,18 +1,42 @@
-﻿using System;
-
-namespace lab10._1
+using SoftDrink;
+using System;
+internal class Program
 {
-    class Program
+   static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            
+        CocaCola cocaCola = new CocaCola();
+        Console.WriteLine("/Кока-Кола/");
+        cocaCola.PrintDrink();
+        cocaCola.PrintCocaCola();
+        Console.WriteLine("\n");
+        cocaCola.PrintCocaColaInfo();
+        cocaCola.PrintDrinkInfo();
+        Console.WriteLine("\n");
 
-            CocaCola cola = new CocaCola();
-            Console.WriteLine("Кола");
-            cola.Input();
-            cola.OutputC();
-           
-        }
+        CocaColaZero cocaColaZero = new CocaColaZero(0, 0, "GABROVO", 0);
+        Console.WriteLine("/Кока-Кола Зеро/");
+        cocaColaZero.PrintDrink();
+        cocaColaZero.PrintCocaColaInfo();
+        cocaColaZero.PrintDrinkInfo();
+        Console.WriteLine("\n");
+
+
+        Fanta fanta = new Fanta(0, 0, 0, "");
+        Console.WriteLine("/Фанта Портокал/");
+        fanta.PrintDrink();
+        fanta.PrintFanta();
+        Console.WriteLine("\n");
+        fanta.PrintFantaInfo();
+        cocaCola.PrintDrinkInfo();
+        Console.WriteLine("\n");
+
+        
+        FantaLemon fantaLemon = new FantaLemon(0, 0, 0, "");
+        Console.WriteLine("/Фанта Лимон/");
+        fantaLemon.PrintDrink();
+        fantaLemon.PrintFanta();
+        Console.WriteLine("\n");
+        cocaCola.PrintDrinkInfo();
+        fantaLemon.PrintFantaInfo();
     }
 }
