32c9e256bf957b9aff7958f7b2a7b3f31342f224
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-05-14 09:42:41 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._3
+{
+    internal class Employee
+    {
+        protected string ime;
+        protected string familia;
+        protected string poziciq;
+        protected string grad;
+
+        public string Ime
+        {
+
+            get { return ime; }
+
+            set { ime = value; }
+        }
+        public string Familia
+        {
+            get { return familia; }
+            set { familia = value; }
+
+        }
+        public string Poziciq
+        {
+            get { return poziciq; }
+            set { poziciq = value; }
+        }
+        public string Grad
+        {
+            get { return grad; }
+            set { grad = value; }
+        }
+        public Employee()
+        {
+
+        }
+        public Employee(string ime, string familia, string poziciq, string grad)
+        {
+            this.ime = ime;
+            this.familia = familia;
+            this.poziciq = poziciq;
+            this.grad = grad;
+
+        }
+
+        public virtual void ValueInfoInput()
+        {
+            Console.WriteLine("Въведи име: ");
+            ime = Console.ReadLine();
+            Console.WriteLine("Въведи възраст: ");
+            ime = Console.ReadLine();
+            Console.WriteLine("Въведи позиция: ");
+            ime = Console.ReadLine();
+            Console.WriteLine("Въведи град: ");
+            ime = Console.ReadLine();
+        }
+        public virtual void ValueInfoOutput()
+        {
+            Console.WriteLine($"{ime}, {}");
+        }
+        public virtual string InfoEmployee()
+        {
+            return "длъжност на служителя";
+        }
+    }
+}
---
+++ Kmet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._3
+{
+    internal class Kmet : Employee
+    {
+        public Kmet(string ime, string familia, string poziciq, string grad) : base(ime, familia, poziciq, grad)
+        {
+
+        }
+        public override void ValueInfo()
+        {
+            Console.WriteLine($"Въведи Кмет-име: {ime} ");
+            Console.WriteLine($"Въведи Кмет-фамилия: {familia}  ");
+            Console.WriteLine($"Въведи Кмет-позиция: {poziciq}  ");
+            Console.WriteLine($"Въведи Кмет-град: {grad}  ");
+
+        }
+        public override string InfoEmployee()
+        {
+            return "Кмет";
+        }
+    }
+}
