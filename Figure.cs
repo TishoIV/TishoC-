338df50cd580d435ce6e08cf652401976c6b6e3c
TishoIV <154275664+TishoIV@users.noreply.github.com>
2026-05-21 09:39:40 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    public abstract class Figure
+    {
+        public abstract void Input();
+        public abstract void Ouput();
+        public abstract double Area();
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Rectangle : Figure
+    {
+        private double height;
+        private double width;
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double h, double w)
+        {
+            height = h;
+            width = w;
+        }
+        public override void Input()
+        {
+            Console.WriteLine("Въведи стойност за височина: ");
+            height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Въведи стойност за широчина: ");
+            width = double.Parse(Console.ReadLine());
+        }
+
+        public override void Ouput()
+        {
+            Console.WriteLine($"Височина е: {height} ");
+            Console.WriteLine($"Широчина е: {width} ");
+
+        }
+        public override double Area()
+        {
+            return height * width;
+        }
+
+    }
+}
